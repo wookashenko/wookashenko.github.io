b5b24c241086fa37448ceb650dbe862871238789
wookashenko <ubartnik22@gmail.com>
2025-06-15 00:50:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 angular.module('cvApp', [])
-.controller('MainController', function($window) {
+.controller('MainController', function($window, $location) {
   const vm = this;
 
   vm.lang = 'pl';
@@ -36,68 +36,109 @@ angular.module('cvApp', [])
       ],
       linkText: 'Naprawione i odnowione przeze mnie urządzenia możesz obejrzeć i zakupić',
       here: 'tutaj',
-      cookieMessage: 'Ta strona wykorzystuje pliki cookies, aby poprawić komfort użytkowania.',
-      accept: 'Akceptuję'
+      cookieMessage: 'Ta strona używa plików cookie w celach informacyjnych. Kontynuując przeglądanie, wyrażasz na to zgodę.',
+      accept: 'OK'
     },
     en: {
       profile: {
         name: 'Łukasz Bartnik',
-        title: 'Junior Specialist in Mobile Device Refurbishment'
+        title: 'Junior refurb specialist of mobile devices'
       },
       sections: [
-        { id: 'about-me', title: 'About Me', content: 'Hi, my name is Łukasz and for several months I have been refurbishing mobile devices, mainly Apple phones. I am interested in sports, politics, and automotive topics. In my free time I enjoy playing computer games, watching series, and reading thrillers/crime books. I am precise and careful, I pay attention to detail and the quality of my work. I like learning new things and improving my skills.' },
-        { id: 'education', title: 'Education', tags: ['2022–present: Warsaw University of Life Sciences, Computer Science']},
-        { id: 'experience', title: 'Experience', tags: ['July 2023–October 2024: Display Tester - GSMOK', 'October 2024–January 2025: Junior IT Support Specialist - moreBIT (Symfonia Group)', 'February 2025–present: Junior Specialist in Mobile Device Refurbishment - Bolttech Repairs Poland']},
-        { id: 'skills', title: 'Skills', items: [
+        { id: 'o-mnie', title: 'About Me', content: 'Hi, my name is Łukasz and for a few months now I have been dealing with the refurbishment of mobile devices, mainly Apple phones. I am interested in sports, politics and motoring. I like to spend my free time playing computer games, watching series and reading thriller/crime books. I am a precise and accurate person, I care about details and the quality of the work I do. I am eager to learn new things and I like to improve my skills.' },
+        { id: 'wyksztalcenie', title: 'Education', tags: ['2022–currently: Warsaw University of Life Sciences, Computer Science'] },
+        { id: 'doswiadczenie', title: 'Experience', tags: ['july 2023–october 2024: Display tester - GSMOK', 'october 2024–january 2025: Junior helpdesk IT specialist - moreBIT (group Symfonia)', 'february 2025–currently: Junior refurb specialist of mobile devices - Bolttech Repairs Poland']},
+        { id: 'umiejetnosci', title: 'Skills', items: [
           { name: 'Programming', stars: 3 },
-          { name: 'Manual dexterity', stars: 5 },
-          { name: 'Accuracy', stars: 5 },
-          { name: 'Driver license: categories B and T', stars: 5 },
+          { name: 'Manual Dexterity', stars: 5 },
+          { name: 'Precision', stars: 5 },
+          { name: 'Driving License: categories B and T', stars: 5 },
           { name: 'English language', stars: 4 },
           { name: 'Russian language', stars: 2 }
         ]},
-        { id: 'interests', title: 'Interests', items: [
+        { id: 'zainteresowania', title: 'Interests', items: [
           { name: 'Technology', stars: 5 },
           { name: 'Politics', stars: 5 },
           { name: 'Automotive', stars: 4 },
-          { name: 'Sports', stars: 4 },
+          { name: 'Sport', stars: 4 },
           { name: 'History', stars: 4 }
         ]},
-        { id: 'links', title: 'Links', link: 'https://luxtrade.pl/', socials: [
-          { icon: 'images/facebook.svg', href: 'https://www.facebook.com/profile.php?id=100008666132198&locale=en_US' },
+        { id: 'linki', title: 'Links', link: 'https://luxtrade.pl/', socials: [
+          { icon: 'images/facebook.svg', href: 'https://www.facebook.com/profile.php?id=100008666132198&locale=pl_PL' },
           { icon: 'images/x.svg', href: 'https://x.com/wookashenko' }
-        ]}
+        ]},
       ],
-      linkText: 'You can view and buy devices repaired and refurbished by me',
+      linkText: 'Repaired and refurbished devices can be viewed and purchased',
       here: 'here',
-      cookieMessage: 'This website uses cookies to improve your browsing experience.',
-      accept: 'Accept'
+      cookieMessage: 'This website uses cookies for informational purposes. By continuing to browse, you consent to this.',
+      accept: 'OK'
     }
   };
 
-  vm.sections = translations[vm.lang].sections;
-  vm.profile = translations[vm.lang].profile;
-  vm.lang = 'pl';
-  vm.currentSection = vm.sections[0];
-  vm.cookieAccepted = localStorage.getItem('cookieAccepted') === 'true';
+  vm.sections = [];
+  vm.currentSection = null;
+  vm.profile = {};
+  vm.cookieAccepted = false;
 
-  vm.selectSection = function(id) {
-    vm.currentSection = vm.sections.find(section => section.id === id);
-  };
-
+  // Funkcja do przełączania języka
   vm.setLang = function(lang) {
     vm.lang = lang;
-    vm.sections = translations[lang].sections;
-    vm.profile = translations[lang].profile;
-    vm.currentSection = vm.sections[0];
+    vm.loadData();
   };
 
-  vm.translate = function(key) {
-    return translations[vm.lang][key] || '';
+  // Załaduj dane dla aktualnego języka
+  vm.loadData = function() {
+    const data = translations[vm.lang];
+    vm.profile = data.profile;
+    vm.sections = data.sections;
+    // Jeśli aktualna sekcja nie istnieje lub nie jest w nowej liście, ustaw na pierwszą
+    if (!vm.currentSection || !vm.sections.find(s => s.id === vm.currentSection.id)) {
+      vm.currentSection = vm.sections[0];
+      // Ustaw hash w URL na aktualną sekcję
+      $window.location.hash = vm.currentSection.id;
+    }
   };
 
+  // Przełączanie sekcji po kliknięciu w menu
+  vm.selectSection = function(id) {
+    const found = vm.sections.find(s => s.id === id);
+    if (found) {
+      vm.currentSection = found;
+      // Ustaw hash w URL
+      $window.location.hash = id;
+    }
+  };
+
+  // Sprawdzanie, czy akceptowano cookies (prosta symulacja)
   vm.acceptCookies = function() {
     vm.cookieAccepted = true;
-    localStorage.setItem('cookieAccepted', 'true');
   };
+
+  // Tłumaczenie tekstów statycznych
+  vm.translate = function(key) {
+    const keys = key.split('.');
+    let val = translations[vm.lang];
+    keys.forEach(k => val = val ? val[k] : null);
+    return val || key;
+  };
+
+  // Inicjalizacja
+  vm.init = function() {
+    // Sprawdź hash w URL i ustaw sekcję
+    const hash = $window.location.hash.substring(1);
+    vm.lang = 'pl';
+    vm.loadData();
+
+    if (hash) {
+      const found = vm.sections.find(s => s.id === hash);
+      if (found) {
+        vm.currentSection = found;
+      }
+    }
+
+    // Sprawdź cookie, tu na demo zawsze false, możesz dodać localStorage lub cookies
+    vm.cookieAccepted = false;
+  };
+
+  vm.init();
 });
